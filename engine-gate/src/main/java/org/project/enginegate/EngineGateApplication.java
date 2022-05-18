578cd7fd1b44f99f9d795cb44ea8ae3018bd6b8f
GermanPlaxunov <snipe9029quant@gmail.com>
2022-05-18 09:20:46 +0300

    Parent, Gate and Core submodules added.

---
+++ engine-gate/src/main/java/org/project/enginegate/EngineGateApplication.java
@@ -0,0 +1,13 @@
+package org.project.enginegate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EngineGateApplication.class, args);
+    }
+
+}
